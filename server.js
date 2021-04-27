bbb88a48dd8a4782702da0b7772e9503c8ae9bc0
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-04-27 17:57:15 +0700

    fix server.js, and add all endpoint for users

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const socket = require("socket.io");
 const cors = require("cors");
 
 const http = require("http");
+const path = require("path");
+
+// Router
+const router = require("./app/routers");
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -35,10 +39,41 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use("/api/v1", router);
+
+app.use("*", (req, res, next) => {
+  const err = new Error("Page not found");
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.message === "Uploaded file must be png, jpg or jpeg file") {
+    res.status(400).send({
+      status: false,
+      message: err.message,
+    });
+  } else if (err.code === "LIMIT_FILE_SIZE") {
+    res.status(400).send({
+      status: false,
+      message: "File image too large",
+    });
+  } else {
+    res.status(404).send({
+      status: false,
+      message: err.message,
+    });
+  }
+});
 
 httpServer.listen(port, () => {
   console.log(`Server is running on http://${host}:${port}`);
